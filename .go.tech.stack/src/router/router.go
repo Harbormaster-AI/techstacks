a4b390a35e5edc926cef5397cf7275ae95efbb87
Scrum Master <dev@realmethods.com>
2021-04-24 19:47:46 -0500

    added association handlers to router

--- .go.tech.stack/src/router/router.go
+++ .go.tech.stack/src/router/router.go
@@ -23,12 +23,41 @@ func Router() *mux.Router {
     // $className Routes to JSON response formatter first 
     // then to the correct Controller function
 	//----------------------------------------------------------------------------
-    
+
+	//----------------------------------------------------------------------------
+	// Standard CRUD Routers
+	//----------------------------------------------------------------------------    
     router.HandleFunc("/api/${className}/{id}", jsonResponseFormatter.FormatToJSON(${className}Controller.Get${className})).Methods("GET", "OPTIONS")
     router.HandleFunc("/api/${className}", jsonResponseFormatter.FormatToJSON(${className}Controller.GetAll${className})).Methods("GET", "OPTIONS")
-    router.HandleFunc("/api/new${className}", jsonResponseFormatter.FormatToJSON(${className}Controller.Create${className})).Methods("POST", "OPTIONS")
+    router.HandleFunc("/api/New${className}", jsonResponseFormatter.FormatToJSON(${className}Controller.Create${className})).Methods("POST", "OPTIONS")
     router.HandleFunc("/api/${className}/{id}", jsonResponseFormatter.FormatToJSON(${className}Controller.Update${className})).Methods("PUT", "OPTIONS")
-    router.HandleFunc("/api/delete${className}/{id}", jsonResponseFormatter.FormatToJSON(${className}Controller.Delete${className})).Methods("DELETE", "OPTIONS")
+    router.HandleFunc("/api/Delete${className}/{id}", jsonResponseFormatter.FormatToJSON(${className}Controller.Delete${className})).Methods("DELETE", "OPTIONS")
+
+	//----------------------------------------------------------------------------
+	// Single Association Routers
+	//----------------------------------------------------------------------------    
+#set( $includeComposites = false )
+#foreach( $singleAssociation in $classObject.getSingleAssociations( ${includeComposites} ) )
+#set( $roleName = $singleAssociation.getRoleName() )
+#set( $lowercaseRoleName = ${Utils.lowercaseFirstLetter(${roleName})} )
+#set( $childName = $singleAssociation.getType() )
+#set( $lowercaseChildName = ${Utils.lowercaseFirstLetter(${childName})} )
+#set( $parentName  = $classObject.getName() )
+    router.HandleFunc("/api/Assign${roleName}/{parentId}/${lowercaseRoleName}Id", jsonResponseFormatter.FormatToJSON(${className}Controller.Assign${className})).Methods("DELETE", "OPTIONS")
+    router.HandleFunc("/api/Unassign${className}/{parentId}", jsonResponseFormatter.FormatToJSON(${className}Controller.Delete${className})).Unassign("DELETE", "OPTIONS")
+#end
+
+	//----------------------------------------------------------------------------
+	// Multiple Association Routers
+	//----------------------------------------------------------------------------    
+#foreach( $multiAssociation in $classObject.getMultipleAssociations() )
+#set( $roleName = $multiAssociation.getRoleName() )
+#set( $lowercaseRoleName = ${Utils.lowercaseFirstLetter(${roleName})} )
+#set( $childName = $multiAssociation.getType() )
+#set( $lowercaseChildName = ${Utils.lowercaseFirstLetter(${childName})} )
+#set( $parentName  = $classObject.getName() )
+    router.HandleFunc("/api/Add${roleName}/{parentId}/${lowercaseRoleName}Id", jsonResponseFormatter.FormatToJSON(${className}Controller.Add${className})).Methods("DELETE", "OPTIONS")
+    router.HandleFunc("/api/Remove${className}/{parentId}/${lowercaseRoleName}Ids", jsonResponseFormatter.FormatToJSON(${className}Controller.Delete${className})).Remove("DELETE", "OPTIONS")    
 #end ##foreach( $class in $classes )
     return router
 }
