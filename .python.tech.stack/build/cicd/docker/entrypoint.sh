d06cc766045f15bda03e087979f2dd51b28fb93e
Steven Randolph <steven.randolph@realmethods.com>
2021-05-25 21:57:36 -0500

    Delete entrypoint.sh

--- .python.tech.stack/build/cicd/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#set( $userId = $aib.getParam("application.adminUserId") )
-#set( $email = $aib.getParam("application.email") )
-#set( $password = $aib.getParam("application.adminPassword") )
-#!/bin/bash
-
-cd /var/www/
-python manage.py migrate --run-syncdb
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${userId}', '${email}', '${password}')"
-python manage.py runserver 0.0.0.0:8080
