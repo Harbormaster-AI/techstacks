9bb34e4d2fe831167235108ccca93a7bb8b08b86
Steven Randolph <dev@harbormaster.ai>
2021-11-25 16:28:03 -0600

    modified test for entity store type in use

--- AxonMongo/framework/app/Application.java
+++ AxonMongo/framework/app/Application.java
@@ -1,9 +1,5 @@
 #header()
 #set( $packageName = $aib.getRootPackageName() )
-#set( $mongoEntityStoreInUse = false )
-#if( $aib.getParam( "axon-framework.using-mongodb-as-entity-store") == "true" )
-#set( $mongoEntityStoreInUse = true )
-#end
 package ${packageName};
 
 import org.springframework.boot.SpringApplication;
@@ -11,14 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.ApplicationContext;
-#if ( $mongoEntityStoreInUse == true )
+#if( $aib.getParam( "axon-framework.entity-store-type") == "mongodb" )
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-#end##if ( $mongoEntityStoreInUse == true )
+#else
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;	
+#end##if( $aib.getParam( "axon-framework.entity-store-type") == "mongodb" )
+	
 @SpringBootApplication
 @ComponentScan(basePackages = "${packageName}.*")
-#if ( $mongoEntityStoreInUse == true )
+#if( $aib.getParam( "axon-framework.entity-store-type") == "mongodb" )
 @EnableMongoRepositories(basePackages = "${packageName}.repository")
-#end##if ( $mongoEntityStoreInUse == true )
+#else
+@EnableJpaRepositories(basePackages = "${packageName}.repository")
+#end##if( $aib.getParam( "axon-framework.entity-store-type") == "mongodb" )
 public class Application {
 
     public static void main(String[] args) {
