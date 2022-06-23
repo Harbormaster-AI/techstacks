4424b3d22eec57276cc34c93c53ed1d86ff2face
Harbormaster Dev Team <dev@harbormaster.ai>
2022-06-23 14:15:00 -0700

    initial commit

---
+++ foundational/.corda.core.tech.stack/helper/CordaConstants.java
@@ -0,0 +1,9 @@
+#header()
+package ${aib.getRootPackageName(true)}.helper.corda;
+
+public interface CordaConstants {
+    String CORDA_USER_NAME = "config.rpc.username";
+    String CORDA_USER_PASSWORD = "config.rpc.password";
+    String CORDA_NODE_HOST = "config.rpc.host";
+    String CORDA_RPC_PORT = "config.rpc.port";
+}
